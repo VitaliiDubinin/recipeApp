26d7adbc2b92950b984bff3071b82d2a5a18bdf7
VitaliiDubinin <vitalii.dubinin@edu.bc.fi>
2022-06-01 17:28:59 +0300

    "style" aboutPage

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,14 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      That is the first stage of RecipeApp. RecipeApp - it will be Collection of Recipes, adapted for children. It was made as a part of education in
-      Business College Helsinki (www.bc.fi). Full Stack Development programm. Thanks a lot our teachers, and especially Margit.
+      That is the first stage of RecipeApp. <br />
+      RecipeApp - it will be Collection of Recipes, adapted for children.
+      <br />
+      It was made as a part of education in Business College Helsinki (www.bc.fi).
+      <br />
+      Full Stack Development programm.
+      <br />
+      Thanks a lot our teachers, and especially Margit.
     </div>
   );
 };
