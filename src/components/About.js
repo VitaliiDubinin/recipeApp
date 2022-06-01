a7aa0fba76de5b9a29a00cd4fb059a49324f3955
VitaliiDubinin <vitalii.dubinin@edu.bc.fi>
2022-06-01 17:13:43 +0300

    about corrected

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const About = () => {
-  return <div>About project page</div>;
+  return (
+    <div>
+      That is the first stage of RecipeApp. RecipeApp - it will be Collection of Recipes, adapted for children. It was made as a part of education in
+      Business College Helsinki (www.bc.fi). Full Stack Development programm. Thanks a lot our teachers, and especially Margit.
+    </div>
+  );
 };
 
 export default About;
